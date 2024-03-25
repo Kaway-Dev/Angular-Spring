d0f40ae81c460228b89eae8991d7d311b5cbb5d0
Philippe <philippekaway@gmail.com>
2024-03-24 23:01:28 -0300

    feat: rxjs e chamada HTTP Get

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,17 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
+import { take, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '../../../assets/cursos.json';
+
   constructor(private httpCliente: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'Front-end' }
-    ];
+  list() {
+    return this.httpCliente.get<Course[]>(this.API)
+      .pipe(
+        take(1),
+        tap(courses => console.log(courses))
+      );
   }
 }
